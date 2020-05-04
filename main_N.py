23f2e6919d7c5b0cb5b885ab8dbfc643c7781a3d
lcol3117 <landoncol3117@gmail.com>
2020-05-04 15:47:17 +0000

    add support for n

---
+++ main_N.py
@@ -0,0 +1,5 @@
+import D3AC_N
+mydata = [[5,10],[4,10],[5,9],[5,11],[3,10],[6,10],[20,15],[19,15],[21,15],[20,14],[20,16],[19,16],[19,14]]
+result=D3AC_N.cluster(mydata,2)
+print(result)
+print("Got {} clusters total. ".format(len(result)))
